383751861df14c3189bd3bc26bb6f30cd72449fc
zijian <zjzhou@sharePC.(none)>
2014-05-07 19:20:26 +0800

    update startInstall.sh

--- startInstall.sh
+++ startInstall.sh
@@ -6,3 +6,4 @@ sudo aptitude install gnome-tweak-tool git build-essential ktouch
 sudo aptitude install dump
 sudo aptitude install chmsee
 sudo aptitude install gconf-editor sam2p
+sudo aptitude install cvs rcs
